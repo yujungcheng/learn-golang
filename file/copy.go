5a9c2794b66defaf09d8ab86360e45fdd0f46327
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-04-15 15:56:11 +1000

    add example

--- file/copy.go
+++ file/copy.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -10,6 +11,8 @@ import (
 func main() {
 	fromFile := "./new_file.txt"
 	toFile := "old_file.txt"
+
+	// method1: io.Copy
 	from, err := os.Open(fromFile)
 	if err != nil {
 		log.Fatal(err)
@@ -23,8 +26,42 @@ func main() {
 	defer to.Close() // close file after all other execution finished
 
 	fmt.Printf("Copy files from %v to %v \n", fromFile, toFile)
-	_, err = io.Copy(to, from)
+	_, err = io.Copy(to, from) // do copy
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// method2: ioutil.WriteFile() and ioutil.ReadFile()
+	input, err := ioutil.ReadFile(fromFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	toFile = "old_file2.txt"
+	err = ioutil.WriteFile(toFile, input, 0644)
+	if err != nil {
+		fmt.Println("Error creating", toFile)
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Copy files from %v to %v \n", fromFile, toFile)
+
+	// method3: os.Read() and os.Write()
+	// https://github.com/mactsouk/opensource.com/blob/master/cp3.go
+	/*
+		buf := make([]byte, BUFFERSIZE)
+		for {
+				n, err := source.Read(buf)
+				if err != nil && err != io.EOF {
+						return err
+				}
+				if n == 0 {
+						break
+				}
+
+				if _, err := destination.Write(buf[:n]); err != nil {
+						return err
+				}
+		}
+	*/
 }
