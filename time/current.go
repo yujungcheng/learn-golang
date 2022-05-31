a9b6e21bdb944bf7acac04bee3f29e1d561875b8
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-05-31 10:24:17 +1000

    use Format() in time module

--- time/current.go
+++ time/current.go
@@ -7,4 +7,8 @@ import (
 
 func main() {
 	fmt.Println(time.Now())
+
+	// print in format YYYY-MM-DD hh:mm:ss
+	now := time.Now()
+	fmt.Println(now.Format("2006-01-02 15:04:05")) // full layout: "2006-01-02 15:04:05 -0700 MST"
 }
